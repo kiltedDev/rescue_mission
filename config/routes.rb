52d1ce34258f8c3169bb7a1fbf1d89800f074ca8
Thomas Wilson <twilson.spqr@Sleipnir.local>
2017-08-28 14:10:22 -0400

    adding questions to view

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  resources :questions, only: [:index, :create, :new, :edit, :show, :update]
+
+  root 'questions#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
---
+++ db/migrate/20170828172825_add_question_description.rb
@@ -0,0 +1,5 @@
+class AddQuestionDescription < ActiveRecord::Migration[5.1]
+  def change
+    add_column :questions, :description, :text
+  end
+end
